5c93330f1557045b9c1c35e74f30c984be6a052e
Kristers <kristers.zujevs@limbazugimnazija.lv>
2026-02-23 13:08:16 +0200

    Add GitHub Actions CI and .dockerignore
    Add a CI workflow to restore, build, publish the .NET 7 project and run a local Docker build (tags: estore:ci) using actions/setup-dotnet and docker/build-push-action. Add a .dockerignore to exclude build artifacts and local files (bin/, obj/, .vs/, .git, Dockerfile, publish/, etc.). Note: obj/Debug files (assembly informational version and inputs cache) changed as a result of the build artifacts.

--- Models.cs
+++ Models.cs
@@ -4,6 +4,7 @@ public class User
 {
     public int Id { get; set; }
     public required string Email { get; set; } = string.Empty;
+    public string? PasswordHash { get; set; }
 }
 
 public class Category
